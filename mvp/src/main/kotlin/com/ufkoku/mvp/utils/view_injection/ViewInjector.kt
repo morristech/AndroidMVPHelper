6fc734dcf183e0de33d810a284010d04848e4b24
Zwei <sergeonnikotos@yandex.ua>
2017-06-16 22:50:22 +0300

    Contining of development.

--- mvp/src/main/kotlin/com/ufkoku/mvp/utils/view_injection/ViewInjector.kt
+++ mvp/src/main/kotlin/com/ufkoku/mvp/utils/view_injection/ViewInjector.kt
@@ -4,19 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ufkoku.mvp.utils.view_injection.annotation.InjectView
 import com.ufkoku.mvp.utils.view_injection.annotation.Layout
 import java.lang.reflect.Field
 import java.util.*
 
 object ViewInjector {
 
-    private val cache: WeakHashMap<Class<*>, MutableList<Field>> = WeakHashMap()
+    private val cache: WeakHashMap<Class<*>, List<Field>> = WeakHashMap()
 
     fun checkAnnotation(target: Any): Boolean {
         return target.javaClass.isAnnotationPresent(Layout::class.java)
     }
 
-    fun injectViews(context: Context, target: Any, parent: ViewGroup?): View {
+    fun injectViews(context: Context, target: Any, cancelOnSuperClass: Class<*>? = null, parent: ViewGroup?): View {
         val layoutId = target.javaClass.getAnnotation(Layout::class.java).layout
         val layout = LayoutInflater.from(context).inflate(layoutId, parent, false)
 
@@ -29,23 +30,31 @@ object ViewInjector {
 
     }
 
-//    /**
-//     * Get all fields, including inherited.
-//     *
-//     * @param typeOfFields - returns fields, which are instance of this param value;
-//     * @param cancelOnSuperClass - stops checking hierarchy, when reaches this class. This class is not checked.
-//     *
-//     * */
-//    private fun Class<*>.getAllAcceptableFields(cancelOnSuperClass: Class<*>? = null): Collection<Field> {
-//        var cacheForClass = cache[this]
-//        if (cacheForClass == null) {
-//            cacheForClass = ArrayList()
-//            cache[this] = cacheForClass
-//        }
-//
-//        fields.forEach { it.isAccessible = true }
-//
-//
-//    }
+    /**
+     * Get all fields, including inherited.
+     *
+     * @param typeOfFields - returns fields, which are instance of this param value;
+     * @param cancelOnSuperClass - stops checking hierarchy, when reaches this class. This class is not checked.
+     *
+     * */
+    private fun Class<*>.getAllAcceptableFields(cancelOnSuperClass: Class<*>? = null): Collection<Field> {
+        var cacheForClass = cache[this]
+        if (cacheForClass == null) {
+
+            cacheForClass = this.declaredFields
+                    .filter { View::class.java.isAssignableFrom(it.javaClass) && it.isAnnotationPresent(InjectView::class.java) }
+                    .toMutableList()
+
+            if (this != cancelOnSuperClass && this.superclass != cancelOnSuperClass) {
+                cacheForClass.addAll(this.superclass.getAllAcceptableFields(cancelOnSuperClass))
+            }
+
+            cache[this] = cacheForClass
+
+            return cacheForClass;
+        }
+
+        return cacheForClass
+    }
 
 }
